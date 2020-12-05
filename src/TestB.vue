67073fecf5e42113a59a8dae4ace582125040c7c
sapphi-red <green@sapphi.red>
2020-12-05 22:55:11 +0900

    repro

--- src/TestB.vue
+++ src/TestB.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
     {{ foo }}
-    <test-a />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import * as _ from 'lodash'
-import TestA from '/@src/TestA.vue'
 
 export default defineComponent({
-  components: { TestA },
   props: ['foo', 'bar']
 })
 </script>
